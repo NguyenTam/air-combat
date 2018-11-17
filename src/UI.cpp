5ce7c002339493464bb9b4272a0e251f94b7c41c
westerl2 <lauri.westerholm@aalto.fi>
2018-11-17 20:10:23 +0200

    Added message boxes

--- src/UI.cpp
+++ src/UI.cpp
@@ -13,8 +13,12 @@
 
 /*  Constructor */
 
-UI::UI(sf::RenderWindow *render_window, sf::Color backgroundcolor):
-window(render_window), BackgoundColor(backgroundcolor) {}
+UI::UI(sf::RenderWindow &render_window, sf::RenderWindow &dialog, sf::Color backgroundcolor):
+window(render_window), dialog_window(dialog), BackgoundColor(backgroundcolor)
+{
+  // TODO error checking
+  messagebox.message_font.loadFromFile(FONT_ARIAL);
+}
 
 
 /*  Return window_status  */
@@ -24,6 +28,15 @@ int UI::getStatus ()
   return window_status;
 }
 
+/* Close active dialog */
+
+void UI::CloseDialog()
+{
+  dialog_window.close();
+  dialog_active = false;
+  messagebox.active = false;
+}
+
 
 /*  Update UI based on sf::Events */
 
@@ -31,46 +44,87 @@ void UI::updateUI()
 {
   sf::Event event;
 
-  // Poll window events
+  /* Poll window events ( if events aren't polled window will freeze, so events
+    are also polled when dialog_window is active) */
 
-  while( window->pollEvent(event) )
+  while( window.pollEvent(event) )
   {
-    switch (event.type)
+    // Only update if there is no active dialog
+    if (! dialog_active)
     {
-      case sf::Event::Closed:
-        window->close();
-        window_status = false;
-        break;
+      switch (event.type)
+      {
+        case sf::Event::Closed:
+          window.close();
+          window_status = false;
+          break;
 
-      case sf::Event::KeyPressed:
-        HandleKeyPress(event);
-        break;
+        case sf::Event::KeyPressed:
+          HandleKeyPress(event);
+          break;
 
-      case sf::Event::MouseMoved:
-        HandleMouseMove(event);
-        break;
+        case sf::Event::MouseMoved:
+          HandleMouseMove(event);
+          break;
 
-      case sf::Event::MouseButtonPressed:
-        HandleMousePress(event);
-        break;
+        case sf::Event::MouseButtonPressed:
+          HandleMousePress(event);
+          break;
 
-      case sf::Event::Resized:
-        HandleResize(event);
-        break;
+        case sf::Event::Resized:
+          HandleResize(event);
+          break;
 
-      default:
-        std::cout << "Other sf::Event" << std::endl;
+        default:
+          std::cout << "Other sf::Event" << std::endl;
+      }
+
+      // Clear and display window
+      window.clear(sf::Color(BackgoundColor));
+      DrawUI();
+      window.display();
+
     }
 
   }
 
-  // Clear and display window
-  window->clear(sf::Color(BackgoundColor));
-  DrawUI();
-  window->display();
+  // Handle also possible dialog_window event loop
+  if (dialog_active)
+  {
+    sf::Event event;
+    while (dialog_window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        CloseDialog();
+      }
+      else if (event.type == sf::Event::MouseButtonPressed)
+      {
+        HandleDialogMousePress(event);
+      }
+      else if (event.type == sf::Event::MouseMoved)
+      {
+        HandleDialogMouseMove(event);
+      }
+    }
+    dialog_window.clear(sf::Color::White);
+    DrawDialog();
+    dialog_window.display();
+  }
 
 }
 
+/* Draw to dialog_window */
+
+void UI::DrawDialog()
+{
+  if (messagebox.active)
+  {
+    // Draw messagebox
+    dialog_window.draw(messagebox.message);
+    dialog_window.draw(messagebox.button);
+  }
+}
 
 /* Handle window resizing */
 
@@ -78,6 +132,78 @@ void UI::HandleResize(sf::Event event)
  {
    // Reset view
    sf::FloatRect new_screen = sf::FloatRect(0, 0, event.size.width, event.size.height);
-   window->setView(sf::View(new_screen));
+   window.setView(sf::View(new_screen));
+
+ }
+
+/* Handle dialog_window mouse press */
+
+ void UI::HandleDialogMousePress(sf::Event event)
+ {
+   if (event.mouseButton.button == sf::Mouse::Left)
+   {
+     float x = (float) event.mouseButton.x;
+     float y = (float) event.mouseButton.y;
+
+     if (messagebox.active)
+     {
+       // if messagebox button is clicked, it calls UI::CloseDialog
+       messagebox.button.checkClicked(x, y);
+     }
+   }
+ }
+
+ /* Handle dialog_window mouse movement */
+
+ void UI::HandleDialogMouseMove(sf::Event event)
+ {
+   float x = (float) event.mouseMove.x;
+   float y = (float) event.mouseMove.y;
+
+   if (messagebox.active)
+   {
+     // Check if mouse hovers on the messagebox
+     messagebox.button.tryActivate(x, y);
+   }
+ }
+
+
+/*  Create and start showing a messagebox */
+
+ void UI::ShowMessageBox(std::string message, sf::Vector2i position)
+ {
+   float button_width = 40;
+   float button_height = 30;
+   messagebox.active = true;
+   messagebox.message = sf::Text(message, messagebox.message_font, 14);
+   messagebox.button = Button("Ok", sf::Color::White, button_width, button_height);
+
+   messagebox.button.setTextColor(sf::Color::Black);
+   messagebox.button.setOutline(1, sf::Color::Black);
+   messagebox.button.setActiveColor(sf::Color(0, 0, 200, 50));
+
+   sf::Text& button_text = messagebox.button.getText();
+   button_text.setStyle(sf::Text::Regular);
+
+   // Bind CloseDialog to Button click_action
+   messagebox.button.setClickFunction( std::bind(&UI::CloseDialog, this) );
+
+   // Get Text rect size
+   float width = messagebox.message.getLocalBounds().width;
+   float height = messagebox.message.getLocalBounds().height;
+   unsigned x = (unsigned) width + button_width + 30; // Modify this if needed
+   unsigned y = (unsigned) height + button_height + 50; // Modify this if needed
+
+   // Create messagebox to the dialog_window
+   dialog_window.create(sf::VideoMode(x, y), "Information", sf::Style::Close);
+   dialog_window.setPosition(position);
+
+   messagebox.message.setPosition(30, 20);
+   messagebox.button.setPosition(x - (button_width + 30), y - (button_height + 10));
+
+   messagebox.message.setFillColor(sf::Color::Black);
+
+   // Change dialog_window status
+   dialog_active = true;
 
  }
--- src/UI.hpp
+++ src/UI.hpp
@@ -1,6 +1,6 @@
 /**
   *   @file UI.hpp
-  *   @brief Header for UI class
+  *   @brief Header for UI class and structs related to it
   */
 
 #pragma once
@@ -13,6 +13,24 @@
 #include <SFML/System.hpp>
 #include "spdlog/spdlog.h"
 #include <iostream>
+#include "button.hpp"
+
+
+
+/**
+  *   @struct MessageBox
+  *   @brief Struct used to display message boxes
+  */
+
+struct MessageBox
+{
+  bool active = false; /**< Whether MessageBox is active or not */
+  Button button; /**< Button for closing the MessageBox */
+  sf::Text message; /**< Text object drawn to MessageBox */
+  sf::Font message_font; /**< Font used to draw MessageBoxes */
+};
+
+
 
 
 /**
@@ -28,10 +46,11 @@ class UI
     /**
       *   @brief Constructor for UI
       *   @param render_window window for UI
+      *   @param dialog Another RenderWindow which is used to show dialogs
       *   @param backgroundcolor UI BackgoundColor
-      **  @remark render_window should probably be a stack object in the main
+      **  @remark render_window should be a stack object in the main
       */
-    UI(sf::RenderWindow *render_window, sf::Color backgroundcolor);
+    UI(sf::RenderWindow &render_window, sf::RenderWindow &dialog, sf::Color backgroundcolor);
 
     /**
       *   @brief Update UI
@@ -46,6 +65,11 @@ class UI
       */
     int getStatus();
 
+    /**
+      *   Close active dialog
+      */
+    void CloseDialog();
+
 
   protected:
 
@@ -56,35 +80,74 @@ class UI
     virtual void DrawUI() = 0;
 
     /**
+      *   @brief Draw current dialog to dialog_window
+      *   @remark Can be reimplemented in a lower class
+      */
+    virtual void DrawDialog();
+
+    /**
       *   @brief Handle incoming key presses
+      *   @param event SFML event
       *   @remark Pure virtual method, implemented in a lower class
       */
     virtual void HandleKeyPress(sf::Event event) = 0;
 
     /**
       *   @brief Handle mouse movement
+      *   @param event SFML event
       *   @remark Pure virtual method, implemented in a lower class
       */
     virtual void HandleMouseMove(sf::Event event) = 0;
 
     /**
       *   @brief Handle mouse presses
+      *   @param event SFML event
       *   @remark Pure virtual method, implemented in a lower class
       */
     virtual void HandleMousePress(sf::Event event) = 0;
 
     /**
       *   @brief Handle sf::Window resizing
+      *   @param event SFML event
       *   @remark Can be reimplemented in a lower class
       */
     virtual void HandleResize(sf::Event event);
+
+    /**
+      *   @brief Handle dialog_window mouse presses
+      *   @param event SFML event
+      *   @remark Can be reimplemented in a lower class
+      */
+    virtual void HandleDialogMousePress(sf::Event event);
+
+    /**
+      *   @brief Handle dialog_window mouse movement
+      *   @param event SFML event
+      *   @remark Can be reimplemented in a lower class
+      */
+    virtual void HandleDialogMouseMove(sf::Event event);
+
+    /**
+      *   @brief Show a message box
+      *   @param message Message shown in the message box
+      *   @param position Initial position for the message box
+      *   @bug The created message box isn't locked top of the main window.
+      *   This can cause box checking to be fustrating. Fixing the issue seems
+      *   complicated and needs low level os window tuning.
+      *   @remark All positions related values used within the method are just
+      *   result of trial and error to make the box look ok
+      */
+    void ShowMessageBox(std::string message, sf::Vector2i position);
 
 
 
     /*  Variables */
 
-    sf::RenderWindow *window;
+    sf::RenderWindow &window; /**< Main window */
+    sf::RenderWindow &dialog_window; /**< Window used to display dialogs */
+    bool dialog_active = false; /**< Whether a dialog is active or not */
     int window_status = 1; /**< 1 window active, 0 closed */
-    sf::Color BackgoundColor;
+    sf::Color BackgoundColor; /**< Background color */
+    struct MessageBox messagebox; /**< MessageBox instance */
 
 };
