391bb4b123ce1f74ceb73fe75c39d6349059f1e6
westerl2 <lauri.westerholm@aalto.fi>
2018-11-25 21:41:09 +0200

    New class to handle basic text input boxes

---
+++ src/TextInput.cpp
@@ -0,0 +1,311 @@
+/**
+  *   @file TextInput.cpp
+  *   @brief Source for class TextInput
+  */
+
+#include "TextInput.hpp"
+
+
+/*  Class TextInput */
+
+
+/*  Empty constructor */
+TextInput::TextInput(): sf::Drawable()
+{
+  width = 0;
+  height = 0;
+  x = 0;
+  y = 0;
+  sf::Color text_color = sf::Color::Black;
+  input = "";
+  font.loadFromFile("../data/fonts/Courier.ttf");
+  text = sf::Text(input, font, font_size);
+  text.setColor(text_color);
+  active_color = sf::Color(200, 200, 200);
+  std_color = sf::Color::White;
+  CreateSanitizationSet();
+  CreateTextInput();
+}
+
+
+/*  Standard constructor */
+TextInput::TextInput(float x, float y, float width, float height): sf::Drawable()
+{
+  this->x = x;
+  this->y = y;
+  this->width = width;
+  this->height = height;
+  sf::Color text_color = sf::Color::Black;
+  input = "";
+  font.loadFromFile("../data/fonts/Courier.ttf");
+  text = sf::Text(input, font, font_size);
+  text.setColor(text_color);
+  text.setPosition(x, y);
+  frame.setOutlineColor(sf::Color::Black);
+  active_color = sf::Color(200, 200, 200);
+  std_color = sf::Color::White;
+
+  CreateSanitizationSet();
+  CreateTextInput();
+}
+
+/* Copy constructor */
+TextInput::TextInput(const TextInput &text_input)
+{
+  x = text_input.x;
+  y = text_input.y;
+  width = text_input.width;
+  height = text_input.height;
+  font = text_input.font;
+  font_size = text_input.font_size;
+  input = text_input.input;
+  CreateTextInput();
+  // Copy chars_on_lines
+  int i = 0;
+  for (auto it = text_input.chars_on_lines.begin(); it != text_input.chars_on_lines.end(); it++)
+  {
+    chars_on_lines[i] = *it;
+    i++;
+  }
+  text_color = text_input.text_color;
+  text = sf::Text(input, font, font_size);
+  text.setColor(text_color);
+  active_color = text_input.active_color;
+  std_color = text_input.std_color;
+  highlight = sf::RectangleShape(sf::Vector2f(0.6 * (float) font_size, font_size));
+  CreateSanitizationSet();
+}
+
+/*  Assignment overload */
+TextInput& TextInput::operator=(const TextInput &text_input)
+{
+  x = text_input.x;
+  y = text_input.y;
+  width = text_input.width;
+  height = text_input.height;
+  font = text_input.font;
+  font_size = text_input.font_size;
+  input = text_input.input;
+  CreateTextInput();
+  // Copy chars_on_lines
+  int i = 0;
+  for (auto it = text_input.chars_on_lines.begin(); it != text_input.chars_on_lines.end(); it ++)
+  {
+    chars_on_lines[i] = *it;
+    i++;
+  }
+  text_color = text_input.text_color;
+  text = sf::Text(input, font, font_size);
+  text.setColor(text_color);
+  text.setPosition(x, y);
+  active_color = text_input.active_color;
+  std_color = text_input.std_color;
+
+  return *this;
+}
+
+/*  Set position */
+void TextInput::setPosition(float x, float y)
+{
+  this->x = x;
+  this->y = y;
+  rect = sf::Rect<float>(x, y, width, height);
+  frame.setPosition(x, y);
+  text.setPosition(x, y);
+}
+
+/*  Draw TextInput */
+void TextInput::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+  target.draw(frame, states);
+  if (active && (line != 1 || chars_on_lines[0] != 0))
+  {
+    target.draw(highlight, states);
+  }
+  target.draw(text, states);
+}
+
+/*  Activate / deactivate */
+bool TextInput::tryActivate(float x, float y)
+{
+  if (rect.contains(x, y))
+  {
+    if (active)
+    {
+      // deactivate
+      active = false;
+      frame.setFillColor(std_color);
+
+    }
+    else
+    {
+      // activate
+      active = true;
+      frame.setFillColor(active_color);
+      return true;
+    }
+  }
+  return false;
+}
+
+/*  Add char to input */
+void TextInput::addChar(char32_t new_char)
+{
+  if (active)
+  {
+    if (new_char < 128)
+    {
+      // Only ascii allowed
+      // Sanitise
+      bool ok = false;
+      if (strict_sanitization)
+      {
+        auto it = input_sanitization_strict.find(new_char);
+        if (it == input_sanitization_strict.end())
+        {
+          ok = true;
+        }
+      }
+      else
+      {
+        auto it = input_sanitization.find(new_char);
+        if (it == input_sanitization.end())
+        {
+          ok = true;
+        }
+      }
+      if (ok)
+      {
+        // Uset input ok
+        if (new_char == 8)
+        {
+          // BackSpace
+          removeChar();
+        }
+        // Notice: following works only for Courier font
+        else if ((0.575 * font_size * (float)chars_on_lines[line -1]) < width)
+        {
+          // Add the char to current line
+          input += new_char;
+          text.setString(input);
+          chars_on_lines[line -1] += 1; // Add one char to correct line
+        }
+        else if (line < (int) (height - font_size) / font.getLineSpacing(font_size))
+        {
+          // Move to the next line
+          line ++;
+          input += '\n';
+          input += new_char;
+          text.setString(input);
+          chars_on_lines[line -1] += 1;
+        }
+      }
+
+    }
+    HighlightCurrent();
+  }
+
+}
+
+/*  Deactivate */
+void TextInput::deactivate()
+{
+  active = false;
+  frame.setFillColor(std_color);
+}
+
+/*  Set outline */
+void TextInput::setOutline(sf::Color color, float thickness)
+{
+  frame.setOutlineColor(color);
+  frame.setOutlineThickness(thickness);
+}
+
+/*  Set text_color */
+void TextInput::setTextColor(sf::Color color)
+{
+  text_color = color;
+  text.setColor(text_color);
+}
+
+
+/*  Remove previous char */
+void TextInput::removeChar()
+{
+  if (chars_on_lines[line -1] > 0)
+  {
+    input.pop_back();
+    chars_on_lines[line -1] -= 1;
+  }
+  else if (input.length() >= 2)
+  {
+    // Remove line feed also
+    input.pop_back();
+    input.pop_back();
+    line --;
+    chars_on_lines[line -1] -= 1;
+  }
+  text.setString(input);
+
+}
+
+/*  Higlight current char */
+void TextInput::HighlightCurrent()
+{
+
+  if (chars_on_lines[line -1] == 0 && line > 1)
+  {
+    highlight.setPosition(x + (float) (chars_on_lines[line -2] - 1) * font_size * 0.575,
+                            y + ((line - 2) * font.getLineSpacing(font_size)) + font_size / 4);
+  }
+  else
+  {
+    highlight.setPosition(x + (float) (chars_on_lines[line -1] - 1) * font_size * 0.575,
+                            y + ((line - 1) * font.getLineSpacing(font_size)) + font_size / 4);
+  }
+
+}
+
+
+/*  Create input_sanitization */
+void TextInput::CreateSanitizationSet()
+{
+  // Set contains integer values for ascii chars which should be sanitized
+  input_sanitization = {0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+  18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 47, 92, 127};
+  // Strict input sanitization set for filenames
+  input_sanitization_strict = {0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15,
+  16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+  36, 37,38, 39, 40, 41, 42, 43, 44, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92,
+  93, 94, 96, 123, 124, 125, 126, 127};
+}
+
+
+/* Enable / disable strict_sanitization */
+void TextInput::enableStrictSanitization(bool activate)
+{
+  strict_sanitization = activate;
+}
+
+/*  Do basic initializations */
+void TextInput::CreateTextInput()
+{
+  rect = sf::Rect<float>(x, y, width, height);
+  frame = sf::RectangleShape(sf::Vector2f(width, height));
+  frame.setPosition(x, y);
+
+  highlight = sf::RectangleShape(sf::Vector2f(0.575 * (float) font_size, font_size));
+  highlight.setFillColor(sf::Color(50, 50, 50, 50));
+
+  // Create chars_on_lines vector
+  if (chars_on_lines.size())
+  {
+    chars_on_lines.clear();
+  }
+  int i = 0;
+  while (i < (int) (height - font_size) / font.getLineSpacing(font_size))
+  {
+    chars_on_lines.push_back(0);
+    i ++;
+  }
+}
---
+++ src/TextInput.hpp
@@ -0,0 +1,155 @@
+/**
+  *   @file TextInput.hpp
+  *   @brief Header for class TextInput
+  */
+
+#pragma once
+
+
+/*  Includes */
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Export.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+
+
+/**
+  *   @class TextInput
+  *   @brief Create text inputs
+  *   @details Used in LevelEditor during saving level
+  *   @remark Supports currently only Courier (monospaced) font
+  */
+class TextInput: public sf::Drawable
+{
+  public:
+
+    /**
+      *   @brief Empty constructor
+      */
+      TextInput();
+
+    /**
+      *   @brief Standard constructor
+      */
+      TextInput(float x, float y, float width, float height);
+
+    /**
+      *   @brief Copy constructor
+      *   @param text_input Reference TextInput
+      *   @remark Outline style is not copied !
+      */
+      TextInput(const TextInput &text_input);
+
+    /**
+      *   @brief Assignment overload
+      *   @param text_input Reference TextInput
+      *   @return Returns copy of text_input
+      *   @remark Outline style is not copied !
+      */
+      TextInput& operator=(const TextInput &text_input);
+
+    /**
+      *   @brief Set position
+      *   @param x x coordinate
+      *   @param y y coordinate
+      */
+      void setPosition(float x, float y);
+
+    /**
+      *   @brief Try to activate /deactivate TextInput
+      *   @param x x coordinate
+      *   @param y y coordinate
+      *   @remark If TextInput is activated, deactivates it
+      */
+      bool tryActivate(float x, float y);
+
+    /**
+      *   @brief Add char to TextInput if the input is active
+      *   @param new_char UTF-32 coded char
+      */
+      void addChar(char32_t new_char);
+
+    /**
+      *   @brief Deactivate TextInput
+      */
+      void deactivate();
+
+    /**
+      *   @brief Set outline color and thickness
+      *   @param color Outline color
+      *   @param thickness Outline thickness
+      */
+      void setOutline(sf::Color color, float thickness);
+
+    /**
+      *   @brief Set text color
+      *   @param color New text_color
+      */
+      void setTextColor(sf::Color color);
+
+    /**
+      *   @brief Removes previous char from the input
+      */
+      void removeChar();
+
+    /**
+      *   @brief Start / stop using strict_sanitization
+      *   @param activate If true strict_sanitization is activated
+      */
+      void enableStrictSanitization(bool activate);
+
+
+  protected:
+
+    /**
+      *   @brief Draw TextInput
+      *   @details Defined as a pure virtual function in sf::Drawable
+      *   @param target RenderTarget target
+      *   @param states RenderStates
+      */
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+  private:
+
+    /**
+      *   @brief Highlight current char
+      */
+      void HighlightCurrent();
+
+    /**
+      *   @brief Create input_sanitization set
+      */
+      void CreateSanitizationSet();
+
+    /**
+      *   @brief Do basic initializations
+      */
+      void CreateTextInput();
+
+
+    std::string input;
+    sf::Text text;
+    sf::Font font;
+    float width;
+    float height;
+    float x;
+    float y;
+    sf::Rect<float> rect;
+    sf::RectangleShape frame;
+    sf::RectangleShape highlight;
+    sf::Color active_color;
+    sf::Color std_color;
+    sf::Color text_color;
+    bool active = false;
+    unsigned font_size = 14;
+    int line = 1;
+    std::vector<unsigned> chars_on_lines;
+    std::set<char32_t> input_sanitization;
+    std::set<char32_t> input_sanitization_strict;
+    bool strict_sanitization = false;
+
+};
