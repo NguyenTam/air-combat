480b01ce39eadc09c24c721b42fd78d49c53a4a7
Tam Nguyen <tam.kismif@gmail.com>
2018-11-26 14:44:38 +0200

    GameEngine_test.cpp file to run

---
+++ test/GameEngine_test.cpp
@@ -0,0 +1,13 @@
+/**
+   @file GameEngine_test.cpp
+   @brief Test file for GameEngine class
+ */
+
+#include "../src/GameEngine.hpp"
+
+int main()
+{
+  GameEngine game;
+  game.run();
+  return 0;
+}
