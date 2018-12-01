515c15234828637117d0b4d28513b927779ed6c1
westerl2 <lauri.westerholm@aalto.fi>
2018-12-01 20:19:19 +0200

    Modified to match LevelEditor new constructor and window creation

--- test/LevelEditor_test.cpp
+++ test/LevelEditor_test.cpp
@@ -9,11 +9,16 @@
 int main()
 {
 
-  // Create a RenderWindow
+  // Create empty RenderWindows
   sf::RenderWindow window;
   sf::RenderWindow dialog_window;
-  
-  LevelEditor editor = LevelEditor(window, dialog_window);
+  sf::RenderWindow help_window;
+
+  // Create window, size and name don't matter, createMainScreen handles those
+  window.create(sf::VideoMode(800, 600), "Level Editor", sf::Style::Close);
+  LevelEditor editor = LevelEditor(window, dialog_window, help_window);
+  editor.createMainScreen();
+
   while(editor.getStatus() == 1)
   {
     editor.updateUI();
