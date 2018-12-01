dda190f1f6e5daebbb4497ffee7743bb232edea0
westerl2 <lauri.westerholm@aalto.fi>
2018-12-01 20:18:26 +0200

    Modified to match MainMenu new constructor and window creation

--- test/MainMenu_test.cpp
+++ test/MainMenu_test.cpp
@@ -14,8 +14,13 @@ int main ()
 
   sf::RenderWindow window;
   sf::RenderWindow dialog_window;
+  sf::RenderWindow help_window;
 
-  MainMenu menu = MainMenu(window, dialog_window);
+  // Create window, size and name don't matter, createMainScreen handles those
+  window.create(sf::VideoMode(800, 600), "", sf::Style::Close);
+
+  MainMenu menu = MainMenu(window, dialog_window, help_window);
+  menu.createMainScreen();
 
   while (menu.getStatus() == 1)
   {
