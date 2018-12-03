34489267655a18809107d69f1949c4af2d1bf38e
westerl2 <lauri.westerholm@aalto.fi>
2018-12-03 22:47:37 +0200

    Necessary changes to test file to notify changes in texture loading

--- test/LevelEntity_test.cpp
+++ test/LevelEntity_test.cpp
@@ -19,10 +19,12 @@ int main()
   float x = 3.4;
   float y = 2;
   std::string img_path = "../data/img/infantry.png";
+  sf::Texture texture;
+  texture.loadFromFile(img_path);
 
   for (int i = 0; i < 5; i++)
   {
-    level_entity = LevelEntity(i * x, i * y, 40, 40, img_path);
+    level_entity = LevelEntity(i * x, i * y, 40, 40, texture);
     std::cout << level_entity;
     assert(level_entity.getPosition().x == x*i);
     assert(level_entity.getPosition().y == y*i);
@@ -30,15 +32,18 @@ int main()
 
   std::cout << std::endl;
   std::cout << "Image loading should fail but not crash the program" << std::endl;
-  LevelEntity entity2 = LevelEntity(43, 668, 50, 50, "");
+  sf::Texture texture2;
+  texture2.loadFromFile("../data/img/test");
+  LevelEntity entity2 = LevelEntity(43, 668, 50, 50, texture2);
   entity2.setPosition(100, 459);
   assert(entity2.getPosition() == sf::Vector2f(100, 459));
 
   entity2.setPosition(sf::Vector2f(30, 50));
   assert(entity2.isInside(45, 56));
 
-  LevelEntity entity3 = LevelEntity(212, 44.6, 78, 10, img_path);
+  LevelEntity entity3 = LevelEntity(212, 44.6, 78, 10, texture);
   LevelEntity entity4 = entity3;
+  entity4.setTexture(texture);
 
   assert(entity4.getPosition() == sf::Vector2f(212, 44.6));
 
