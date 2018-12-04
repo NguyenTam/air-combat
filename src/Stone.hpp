fb4f51ecb73a099af71e1321428d58b9ccf0aa68
Lauri Blomberg <lauri.blomberg@aalto.fi>
2018-12-04 16:52:35 +0200

    Lisäsin perusentityjä

---
+++ src/Stone.hpp
@@ -0,0 +1,27 @@
+/**
+ *   @file Stone.hpp
+ *   @brief Header for Stone class
+ */
+#pragma once
+
+/* Includes */
+
+#include <SFML/Graphics.hpp>
+#include "Entity.hpp"
+
+/**
+  *   @class Stone
+  */
+
+class Stone : public Entity {
+  public:
+
+  /**
+   *   @brief Construct a Stone object.
+   *   @param w Gameworld where stone is created in
+   *   @param b stones body for collisions
+   *   @param t stones texture
+   *   @param position Place as vector where stone is created
+   */
+  Stone(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position);
+};
---
+++ src/Tree.hpp
@@ -0,0 +1,28 @@
+/**
+ *   @file Tree.hpp
+ *   @brief Header for Tree class
+ */
+#pragma once
+
+/* Includes */
+
+#include <SFML/Graphics.hpp>
+#include "Entity.hpp"
+
+/**
+  *   @class Tree
+  */
+
+class Tree : public Entity {
+  public:
+
+  /**
+   *   @brief Construct a Tree object.
+   *   @param w Gameworld where tree is created in
+   *   @param b Trees body for collisions
+   *   @param t Trees texture
+   *   @param position Place as vector where tree is created
+   */
+  Tree(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position);
+};
+
