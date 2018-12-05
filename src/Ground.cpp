41e2cce60bf3d4e852a942245ca127ac53de81bd
Lauri Blomberg <lauri.blomberg@aalto.fi>
2018-12-05 13:27:00 +0200

    Hangar olion hpp sekä cpp tiedostot

--- src/Ground.cpp
+++ src/Ground.cpp
@@ -1,4 +1,4 @@
 #include "Ground.hpp"
 
-Ground::Ground(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position):Entity(w, b, t, position, 0.f, 0, 0, 0, 100){ }
+Ground::Ground(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position):Entity(w, b, t, position, 0.f, 0, 0, 0, 99999){ }
 
---
+++ src/Hangar.hpp
@@ -0,0 +1,27 @@
+/**
+ *   @file Hangar.hpp
+ *   @brief Header for Hangar class
+ */
+#pragma once
+
+/* Includes */
+
+#include <SFML/Graphics.hpp>
+#include "Entity.hpp"
+
+/**
+  *   @class Hangar
+  */
+
+class Hangar : public Entity {
+  public:
+
+  /**
+   *   @brief Construct a Hangar object.
+   *   @param w Gameworld where Hangar is created in
+   *   @param b Hangars body for collisions
+   *   @param t Hangars texture
+   *   @param position Place as vector where Hangar is created
+   */
+  Hangar(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position);
+};
