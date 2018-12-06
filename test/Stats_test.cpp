0f0697fbfed8a8f756474cae5e7983c390e149b0
westerl2 <lauri.westerholm@aalto.fi>
2018-12-06 17:51:34 +0200

    Test main for Stats class

---
+++ test/Stats_test.cpp
@@ -0,0 +1,26 @@
+/**
+  *   @file Stats_test.cpp
+  *   @brief Test main for Stats class
+  */
+
+#include "../src/Stats.hpp"
+
+
+/**
+  *   @brief Test main for Stats
+  */
+
+int main()
+{
+  // Create RenderWindow and construct Stats
+  sf::RenderWindow window;
+  window.create(sf::VideoMode(Game::WIDTH, Game::HEIGHT), "Main Menu", sf::Style::Close);
+  Stats stats = Stats(window);
+  
+  // Update Stats until user exits Stats
+  stats.update();
+  // Get ExitStatus from Stats
+  int exit_status = stats.getExitStatus();
+  std::cout << "ExitStatus: " << exit_status << std::endl;
+  return 0;
+}
