42a3210f2668ca961c1a4bb30f7dd0b34b68dc80
Tam Nguyen <tam.kismif@gmail.com>
2018-12-07 19:31:57 +0200

    movements

--- src/AI.hpp
+++ src/AI.hpp
@@ -9,10 +9,9 @@
 namespace AI {
   extern std::map<Game::TYPE_ID, int> priority_list;
 
-
   std::tuple<Game::ACTIONS, sf::Vector2f> get_action(Entity& me, std::list<Entity*> &surroundings);
   std::tuple<Game::ACTIONS, sf::Vector2f> get_airplane_action(Entity& me, std::list<Entity*> &surroundings);
   std::tuple<Game::ACTIONS, sf::Vector2f> get_antiaircraft_action(Entity& me, std::list<Entity*> &surroundings);
   std::tuple<Game::ACTIONS, sf::Vector2f> get_infantry_action(Entity& me, std::list<Entity*> &surroundings);
-  void set_target(Entity& me, std::list<Entity*> &surroundings, Entity* current_worse_enemy, int &current_worse_enemy_priority);
+  void set_target(Entity& me, std::list<Entity*> &surroundings, Entity* current_worse_enemy, int &current_worse_enemy_priority);  
 }
