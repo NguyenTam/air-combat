0114bc28e8ea194176da35a8d9e56841d048c2ba
Tam Nguyen <tam.kismif@gmail.com>
2018-12-07 15:26:26 +0200

    use extern

--- src/AI.cpp
+++ src/AI.cpp
@@ -1,6 +1,18 @@
 #include "AI.hpp"
 namespace AI
 {
+  std::map<Game::TYPE_ID, int> priority_list
+  {
+    { Game::TYPE_ID::airplane, 10 },
+    { Game::TYPE_ID::antiaircraft, 7 },
+    { Game::TYPE_ID::base, 6 },
+    { Game::TYPE_ID::hangar, 5 },
+    { Game::TYPE_ID::infantry, 8 },
+    { Game::TYPE_ID::bullet, 9 },
+    { Game::TYPE_ID::ground, -1 },
+    { Game::TYPE_ID::rock, 1 },
+    { Game::TYPE_ID::tree, 1 }
+  };
   std::tuple<Game::ACTIONS, sf::Vector2f> get_action(Entity& me, std::list<Entity*> &surroundings)
   {
     srand(time(nullptr));
