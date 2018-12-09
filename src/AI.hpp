9651b0852bfc4ee0ebb3ec37408e9bba39e79a98
Tam Nguyen <tam.kismif@gmail.com>
2018-12-09 14:34:16 +0200

    is too close function

--- src/AI.hpp
+++ src/AI.hpp
@@ -2,16 +2,16 @@
 #include "CommonDefinitions.hpp"
 #include "Entity.hpp"
 #include <tuple>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
 #include <list>
+#include <cstdlib>
 
 namespace AI {
   extern std::map<Game::TYPE_ID, int> priority_list;
-
-  std::tuple<Game::ACTIONS, sf::Vector2f> get_action(Entity& me, std::list<Entity*> &surroundings);
-  std::tuple<Game::ACTIONS, sf::Vector2f> get_airplane_action(Entity& me, std::list<Entity*> &surroundings);
-  std::tuple<Game::ACTIONS, sf::Vector2f> get_antiaircraft_action(Entity& me, std::list<Entity*> &surroundings);
-  std::tuple<Game::ACTIONS, sf::Vector2f> get_infantry_action(Entity& me, std::list<Entity*> &surroundings);
-  void set_target(Entity& me, std::list<Entity*> &surroundings, Entity* current_worse_enemy, int &current_worse_enemy_priority);  
+  
+  void get_action(Entity& me, std::list<Entity*> &surroundings);
+  void get_airplane_action(Entity& me, std::list<Entity*> &surroundings);
+  void get_antiaircraft_action(Entity& me, std::list<Entity*> &surroundings);
+  void get_infantry_action(Entity& me, std::list<Entity*> &surroundings);
+  void set_target(Entity& me, std::list<Entity*> &surroundings, sf::Vector2f & current_worse_enemy, int &current_worse_enemy_priority);
+  bool is_too_close(sf::Vector2f & e1, sf::Vector2f & e2);
 }
