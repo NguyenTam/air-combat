d7d36e2323ee296423a649d5bf876984f5d97406
westerl2 <lauri.westerholm@aalto.fi>
2018-12-10 18:23:02 +0200

    Added this to Bullet constructor

--- src/Artillery.cpp
+++ src/Artillery.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 
 //Note about the magical numbers
-//Entity(b2World &w, b2Body &b, const sf::Texture &t, const sf::Vector2f &position, float speed, int bullets, int bombs, int firerate, int hp, sf::Vector2f direct, Game::TEAM_ID team) 
-Artillery::Artillery(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position, Game::TEAM_ID team):Entity(w, b, t, position, 0.f, 1000, 0, 90, 30, sf::Vector2f(1.0f, 0.0f), team){ 
+//Entity(b2World &w, b2Body &b, const sf::Texture &t, const sf::Vector2f &position, float speed, int bullets, int bombs, int firerate, int hp, sf::Vector2f direct, Game::TEAM_ID team)
+Artillery::Artillery(b2World &w,  b2Body &b, const sf::Texture &t, const sf::Vector2f &position, Game::TEAM_ID team):Entity(w, b, t, position, 0.f, 1000, 0, 90, 30, sf::Vector2f(1.0f, 0.0f), team){
   typeId = Game::TYPE_ID::antiaircraft;
   }
 
@@ -52,22 +52,20 @@ bool Artillery::shoot(sf::Vector2f direction, ResourceManager& resources){
           body->CreateFixture(&FixtureDef);
 
           sf::Vector2f pos(x,y);
-          std::shared_ptr<Entity> bullet = std::make_shared<Bullet>(world, *body, tex, pos, sf::Vector2f(1.0f, 0.0f));
+          std::shared_ptr<Entity> bullet = std::make_shared<Bullet>(world, *body, tex, pos, sf::Vector2f(1.0f, 0.0f), this);
           bullet->setType(Textures::Bullet_alpha);
 
           body->SetGravityScale(0.5f);
           body->ApplyLinearImpulse(b2Vec2(direction.x/5, direction.y/5), body->GetWorldCenter(), true);
           body->SetUserData(bullet.get());
-          active_bullets.push_back(bullet);      
-          
+          active_bullets.push_back(bullet);
+
           numberOfBullets-=1;
           clock.restart();
           std::cout << "artillery shot" << std::endl;
-          return true;  
+          return true;
         }
     }
   //fireCountDown-=1;
-    return false;    
+    return false;
 }
-
-
