2878581f9a8504618634434ddc73e05d552510b0
Tam Nguyen <tam.kismif@gmail.com>
2018-12-14 22:36:07 +0200

    change to valid file

--- test/World_test.cpp
+++ test/World_test.cpp
@@ -10,7 +10,7 @@ int main() {
 	sf::RenderWindow help_window;
 		// Create window, size and name don't matter, createMainScreen handles those
 	window.create(sf::VideoMode(800, 600), "", sf::Style::Close);
-	std::string str = "../data/level_files/Level1.txt";
+	std::string str = "../data/level_files/Testi.txt";
 	ResourceManager manager = ResourceManager();
 	World world(window,manager);
 	world.read_level(str, Game::GameMode::SinglePlayer);
