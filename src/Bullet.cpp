0249da03fb8bd914aeea53a607fdff140a3e8ff9
Tam Nguyen <tam.kismif@gmail.com>
2018-12-14 15:24:08 +0200

    fix: consistent naming

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -1,10 +1,10 @@
 #include "Bullet.hpp"
 
-Bullet::Bullet(b2World &w,  b2Body *b, const sf::Texture &t, const sf::Vector2f &position, sf::Vector2f direct):Entity(w, b, t,position, 50, 0, 0, 0, 1, direct, Game::TEAM_ID::all_enemy){
+Bullet::Bullet(b2World &w,  b2Body *b, const sf::Texture &t, const sf::Vector2f &position, sf::Vector2f direction):Entity(w, b, t,position, 50, 0, 0, 0, 1, direction, Game::TEAM_ID::all_enemy){
   typeId = Game::TYPE_ID::bullet;
   }
 
-Bullet::Bullet(b2World &w,  b2Body *b, const sf::Texture &t, const sf::Vector2f &position, sf::Vector2f direct, Entity* owner):Entity(w, b, t,position, 50, 0, 0, 0, 1, direct, Game::TEAM_ID::all_enemy){
+Bullet::Bullet(b2World &w,  b2Body *b, const sf::Texture &t, const sf::Vector2f &position, sf::Vector2f direction, Entity* owner):Entity(w, b, t,position, 50, 0, 0, 0, 1, direction, Game::TEAM_ID::all_enemy){
   typeId = Game::TYPE_ID::bullet;
   this->owner = owner;
   }
